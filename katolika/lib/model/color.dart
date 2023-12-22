2b086782c3ec4042f058974a7ea2f1c0a956c685
Rakotoarisaona Parfait <rak.parfait@outlook.com>
2023-12-22 09:31:04 +0300

    init project

--- katolika/lib/model/color.dart
+++
@@ -1,6 +0,0 @@
-import "package:flutter/material.dart";
-
-const Color tealBlue = Color(0xFF043B5C);
-const Color tealDark = Color(0xFF02253B);
-const Color primary = Color(0xFF545F71);
-const Color secondary = Color(0xFFF0F2F5);

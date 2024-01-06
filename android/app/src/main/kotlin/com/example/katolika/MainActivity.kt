5b97683c7d658f5a0d93eb45c29c467dbf2deaa0
Rakotoarisaona Parfait <rak.parfait@outlook.com>
2024-01-06 13:28:48 +0300

    null

--- android/app/src/main/kotlin/com/example/katolika/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.katolika
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/model/color.dart
+++
@@ -1,6 +0,0 @@
-import "package:flutter/material.dart";
-
-const Color primary = Color(0xFF182C61);
-const Color secondary = Color(0xFF0a3d62);
-const Color primaryDark = Color(0xFF182C61);
-const Color primaryLight = Color(0xFFFFFFFF);
